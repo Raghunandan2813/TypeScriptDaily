68fec94f111d74a549597dc44565be9d92dfb85c
Raghunandan <raghu9555k@gmail.com>
2026-02-21 11:40:28 +0530

    learn interface

---
+++ src/classromm.ts
@@ -0,0 +1,55 @@
+// // here in paramerter object is passing
+
+
+// function student ( user: {
+//     name : string,
+//     age: number
+// }){
+//     console.log("hello" + user.name)
+// }
+
+
+// student({
+//     name: "harkirat",
+//     age: 21
+// })
+
+
+// // Interface
+
+// interface User{
+//     firstName : string,
+//     lastName : string,
+//     email : string,
+//     age : number,
+// }
+
+// // function islegal(user.User){
+// //     if(user.age>18){
+// //         return true;
+// //     }else{
+// //         return false;
+// //     }
+// // }
+
+
+// // intersection
+
+// interface Manager{
+//     name: string,
+//     age : number,
+// }
+
+// interface Department{
+//     name: string,
+//     departmentName: string,
+// }
+
+
+// type TeamLead = Manager & Employee 
+// // 
+// let t : TeamLead = {
+//     name : "harkirat",
+//     age : 21 ,
+//     department: "ASdf"
+// }
---
+++ src/typescript.ts
@@ -0,0 +1,25 @@
+ 
+
+ let subs : number | string = 'M1'
+
+ let apiRequestStatus : 'pending' | 'success' | 'error' = 'pending'
+
+ let airlineSeat : 'aisle' | 'window' | 'middle' = 'asile'
+
+ airlineSeat = 'aisle'
+
+ const orders = ['12' , '20', '42']
+
+ let currentorder : string | undefined;
+
+ for(let order of orders){
+    if(order === '28'){
+        currentorder = order
+        break 
+    }
+ }
+
+ currentorder = 42;
+ console.log(currentorder)
+
+
