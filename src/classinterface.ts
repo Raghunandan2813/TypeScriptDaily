391bb02b1ab07c0ec8037cf4b0e14205c3ab9a51
Raghunandan <raghu9555k@gmail.com>
2026-03-06 11:31:51 +0530

    Added file

--- src/classinterface.ts
+++ src/classinterface.ts
@@ -1,2 +1,26 @@
 
 
+interface  Student {
+    Name: string,
+    Age: number,
+    Address: {
+        District : string,
+        State: string,
+        PinCode : number
+    },
+
+}
+
+let student : Student ={
+    Name : "RAGHUNANDAN",
+    Age: 21,
+    Address : {
+        District : "Kushinagar",
+        State: "Uttar Pradesh",
+        PinCode: 274301
+    }
+}
+
+function isLegal(student : Student) : boolean{
+    return student.Age>18;
+}
