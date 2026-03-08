a89018696019f6b3fc7dd6a6f3ff9b0176d47b9b
Raghunandan <raghu9555k@gmail.com>
2026-03-08 22:49:42 +0530

    Added file

--- src/classromm.ts
+++ src/classromm.ts
@@ -53,3 +53,28 @@
 //     age : 21 ,
 //     department: "ASdf"
 // }
+
+interface Address {
+    city: string,
+    country ?: string,
+    pincode ?: number
+}
+
+interface User {
+    name: string,
+    age : number,
+    address : Address 
+}
+
+
+interface Office {
+    address: Address
+}
+
+let user : User = {
+    name : "Raghunandan Kumar",
+    age : 22,
+    address: {
+        city: "Kushinangar"
+    }
+}
