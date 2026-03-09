b212cb2335ce95599e3d5cdc48eaf4fef71a9504
Raghunandan <raghu9555k@gmail.com>
2026-03-09 21:14:28 +0530

    added file

--- src/greet.ts
+++ src/greet.ts
@@ -142,18 +142,7 @@ onecall(log);
 
 
 
-// function twocall(otherfn : (digit1 : number, digit2:number)=> number, x : setTimeout(()=>{
-//         const result = otherfn(x , y);
-//         console.log(result)
-//     }, 5000)
-// }
 
-
-// function addition(a :number , b: number) :number{
-//     return a + b
-// }
-
-// twocall(addition, 2, 3)
 
 
 function twocall(otherfn: (digit1: number, digit2: number) => number, x: number, y: number) {
