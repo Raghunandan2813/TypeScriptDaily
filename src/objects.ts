b44a8710afb0660c0c911701a367937258590eb5
Raghunandan <raghu9555k@gmail.com>
2026-03-12 19:13:02 +0530

    added file

---
+++ src/objects.ts
@@ -0,0 +1,50 @@
+ 
+
+//  let subs : number | string = 'M1'
+
+//  let apiRequestStatus : 'pending' | 'success' | 'error' = 'pending'
+
+//  //let airlineSeat : 'aisle' | 'window' | 'middle' = 'asile'
+
+//  //airlineSeat = 'aisle'
+
+//  const orders = ['12' , '20', '42']
+
+//  let currentorder : string | undefined;
+
+//  for(let order of orders){
+//     if(order === '28'){
+//         currentorder = order
+//         break 
+//     }
+//  }
+
+//  //currentorder = 42;
+//  console.log(currentorder)
+
+
+ 
+
+ let subs : number | string = 'M1'
+
+ let apiRequestStatus : 'pending' | 'success' | 'error' = 'pending'
+
+ //let airlineSeat : 'aisle' | 'window' | 'middle' = 'asile'
+
+ //airlineSeat = 'aisle'
+
+ const orders = ['12' , '20', '42']
+
+ let currentorder : string | undefined;
+
+ for(let order of orders){
+    if(order === '28'){
+        currentorder = order
+        break 
+    }
+ }
+
+ //currentorder = 42;
+ console.log(currentorder)
+
+
